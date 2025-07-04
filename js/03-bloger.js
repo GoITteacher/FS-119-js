4cc91a441546c2b71141bc54a90143a4894245d2
Volodymyr Pashchenko <volodkaposhta@gmail.com>
2025-07-04 17:20:16 +0300

    update

--- js/03-bloger.js
+++ js/03-bloger.js
@@ -11,21 +11,21 @@
  * - Додай метод updatePostCount(value), який у параметрі value приймає кількість постів, які потрібно додати користувачеві.
  */
 
-const mango = new User({
-  name: "mango@mail.com",
+const mango = new Blogger({
+  name: 'mango@mail.com',
   age: 24,
   numberOfPosts: 20,
-  topics: ["tech", "cooking"],
+  topics: ['tech', 'cooking'],
 });
 console.log(mango.getInfo()); // User mango@mail.com is 24 years old and has 20 posts
 mango.updatePostCount(5);
 console.log(mango.getInfo()); // User mango@mail.com is 24 years old and has 25 posts
 
-const poly = new User({
-  name: "poly@mail.com",
+const poly = new Blogger({
+  name: 'poly@mail.com',
   age: 19,
   numberOfPosts: 17,
-  topics: ["sports", "gaming", "health"],
+  topics: ['sports', 'gaming', 'health'],
 });
 console.log(poly.getInfo()); // User poly@mail.com is 19 years old and has 17 posts
 poly.updatePostCount(4);
