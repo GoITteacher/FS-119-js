6eeb27668401546294edb4747105f7bb3c93fbb0
Volodymyr Pashchenko <volodkaposhta@gmail.com>
2025-07-18 21:12:14 +0300

    update

--- js/01-basic.js
+++ js/01-basic.js
@@ -1,28 +1,162 @@
 /**
  * Деструктуризація об'єкта
  */
+//!======================================================
+// const user = {
+//   username: 'Jacob name',
+//   age: 10,
+//   address: 'Test',
+//   city: 'Dnipro',
+//   skills: {
+//     html: true,
+//     css: false,
+//     js: true,
+//   },
+// };
 
-const user = {
-  username: "Jacob name",
-  skills: {
-    html: true,
-    css: false,
-    js: true,
-  },
-};
+// const { username, age, address, city } = user;
+// const username = user.username;
+// const age = user.age;
+// const address = user.address;
+// const city = user.city;
 
+//!======================================================
 /**
  * Глибока деструктуризація об'єкта
  */
 
-const { skills } = user;
+// const username = 'Vasya';
+
+// const user = {
+//   username: 'Jacob name',
+//   age: 10,
+//   address: {
+//     location: {
+//       x: 100,
+//       y: 200,
+//     },
+//   },
+//   city: 'Dnipro',
+//   skills: {
+//     html: true,
+//     css: false,
+//     js: true,
+//   },
+// };
+
+// const { username: myName } = user;
+// const myName = user.username
+//!======================================================
+// const {
+//   username: name,
+//   skills: { html, css, js },
+//   age,
+// } = user;
+// const {html, css, js} = user.skills;
+
+// const {
+//   address: {
+//     location: { x, y },
+//   },
+// } = user;
 
 // console.log("html", html);
 // console.log("css", css);
 // console.log("js", js);
+//!======================================================
 
+// const userInfo = {
+//   username: 'Roman',
+//   age: 25,
+// };
+
+// const { username = 'Anonym' } = userInfo;
+// const username = unefined || 'Anonym';
+
+//!======================================================
+// const userInfo = {
+//   username: 'Roman',
+//   age: 25,
+// };
+
+// const { username: name = 'Anonym', age: myAge = 0, address } = userInfo;
+
+//!======================================================
+// const user = {
+//   age: 10,
+//   address: {
+//     location: {
+//       x: 100,
+//       y: 200,
+//     },
+//   },
+//   city: 'Dnipro',
+//   skills: {
+//     html: true,
+//     css: false,
+//     js: true,
+//   },
+//   username: 'Jacob name',
+// };
+
+// console.log(user);
+
+// console.log(user.address);
+
+// const { address, skills, ...newUser } = user;
+
+// console.log(newUser);
+
+// const { username, city, city: city1 } = user;
+// const {city, username} = user;
+
+// const city = user.city;
+// const username = user.username;
+//!======================================================
 /**
  * Деструктуризація масива
  */
 
-const arr = [1, 2, 3, 4, 5];
+// const arr = [1, 2, 3, 4, 5];
+// const [x1, x2, x3] = arr;
+
+// const rgb = [250, 110, 15, 0.5];
+// const [red, , blue] = rgb;
+
+// const red = rgb[0];
+// const green = rgb[1];
+// const blue = rgb[2];
+//!======================================================
+
+// const arr = [25, 4, 56, 3, 1, 2, 4, 6, 8, 5, 3, 2, 46, 8];
+
+// const [max, min, ...numbers] = arr;
+
+// console.log(max);
+
+//!======================================================
+
+// let x = 10;
+// let y = 20;
+// [y, x] = [x, y];
+
+// let x = 10;
+// let y = 20;
+// let tmp = x;
+// x = y;
+// y = tmp;
+
+//!======================================================
+
+// const arr = [10, 20];
+// const [x1 + 1,x2] = arr
+//!======================================================
+// let [x1, x2, x3, x4, x5] = [10, 20, 30, 40, 50];
+
+// let x1 = 10;
+// let x2 = 20;
+// let x3 = 20;
+// let x4 = 20;
+// let x5 = 20;
+
+// console.log(x1, y1);
--- js/02-params.js
+++ js/02-params.js
@@ -2,22 +2,72 @@
  * Деструктуризація об'єкта в параметрах функції
  */
 
-const user = {
-  username: "Jacob name",
-  skills: {
-    html: true,
-    css: false,
-    js: true,
-  },
-};
+// const user = {
+//   username: 'Jacob name',
+//   skills: {
+//     html: true,
+//     css: false,
+//     js: true,
+//   },
+// };
 
 // Без деструктуризації
-function getUserName(obj) {
-  console.log(
-    `Hello my name is ${obj.name}, I know html - ${obj.skills.html}, css - ${obj.skills.css} and js -${obj.skills.js}`
-  );
-}
+// function getUserName(obj) {
+//   console.log(
+//     `Hello my name is ${obj.name}, I know html - ${obj.skills.html}, css - ${obj.skills.css} and js -${obj.skills.js}`,
+//   );
+// }
 
 // З деструктуризацією
 
-getUserName(user);
+// function getUserName(y, { name, skills: { html, css, js } }, x) {
+//   console.log(
+//     `Hello my name is ${name}, I know html - ${html}, css - ${css} and js -${js}`,
+//   );
+// }
+
+// getUserName(20, userInfo, 10);
+
+//!======================================================
+
+// function foo(x, y) {}
+// foo(10, 20);
+
+// function createUser(
+//   name = '',
+//   age,
+//   height,
+//   weight,
+//   city,
+//   country,
+//   isStudent,
+//   phoneNumber,
+//   gender,
+// ) {
+//   const obj = {
+//     name,
+//     age,
+//     height,
+//     weight,
+//     city,
+//     country,
+//     isStudent,
+//     phoneNumber,
+//     gender,
+
+//     showInfo() {},
+//   };
+// }
+
+// createUser('Roman', 65, 160, 66, 'Dnipro', 'Ukraine', false, '9234234', 'Male');
+
+// function createUser2({ name = '', age = 0, city, country, height, weight }) {}
+
+// createUser2({
+//   height: 160,
+//   name: 20,
+//   city: 'Dnipro',
+//   country: 'Ukraine',
+//   age: 10,
+//   weight: 65,
+// });
