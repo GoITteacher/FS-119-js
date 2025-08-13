2f71c3699dafc3d675054546f42d9f1b3ae4c0d6
Volodymyr Pashchenko <volodkaposhta@gmail.com>
2025-08-13 21:52:18 +0300

    update

--- src/js/modules/pokemon.js
+++ src/js/modules/pokemon.js
@@ -1,21 +1,46 @@
+import axios from 'axios';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.css';
+
 const refs = {
   formEl: document.querySelector('.js-pokemon-form'),
   listEl: document.querySelector('.js-pokemon-list'),
+  loaderEl: document.querySelector('.js-loader'),
 };
 
-refs.formEl.addEventListener('submit', onFormElSubmit);
-function onFormElSubmit(event) {
-  event.preventDefault();
-  const value = refs.formEl.elements.query.value;
-  getPokemon(value).then(renderPokemon);
+//!======================================================
+
+refs.formEl.addEventListener('submit', async e => {
+  e.preventDefault();
+  const pokemonName = e.target.elements.query.value;
+
+  showLoader();
+
+  try {
+    const pokemon = await fetchPokemon(pokemonName);
+    const markup = pokemonTemplate(pokemon);
+    refs.listEl.insertAdjacentHTML('afterbegin', markup);
+  } catch {
+    iziToast.error({
+      message: 'Invalid pokemon Name!',
+    });
+  }
+
+  hideLoader();
+
+  e.target.reset();
+});
+
+async function fetchPokemon(pokemonName) {
+  const baseUrl = 'https://pokeapi.co/api/v2';
+  const endPoint = `/pokemon/${pokemonName}`;
+  const url = baseUrl + endPoint;
+  const res = await axios.get(url);
+  return res.data;
 }
 
-function getPokemon(value) {
-  const url = `https://pokeapi.co/api/v2/pokemon/${value.toLowerCase()}`;
-  return fetch(url).then(res => {
-    return res.json();
-  });
-}
+//!======================================================
+
 function pokemonTemplate(pokemon) {
   const {
     height,
@@ -44,29 +69,12 @@ function pokemonTemplate(pokemon) {
 </div>`;
 }
 
-function renderPokemon({
-  height,
-  weight,
-  id,
-  name,
-  base_experience,
-  sprites: { front_default, back_default },
-}) {
-  const markup = `<div class="pokemon-card pokemon-item">
-  <h1 class="pokemon-name">${name} - Pokemon Details</h1>
-  <img data-back="${back_default}" data-front="${front_default}"
-    class="pokemon-image js-pocimage"
-    src="${front_default}"
-    alt="${name}"
-  />
+//!======================================================
 
-  <h2 class="section-title">Basic Information</h2>
-  <ul class="info-list">
-    <li>ID: ${id}</li>
-    <li>Height: ${height} decimetres</li>
-    <li>Weight: ${weight} grams</li>
-    <li>Base Experience: ${base_experience}</li>
-  </ul>
-</div>`;
-  refs.listEl.insertAdjacentHTML('beforeend', markup);
+function showLoader() {
+  refs.loaderEl.classList.remove('hidden');
+}
+
+function hideLoader() {
+  refs.loaderEl.classList.add('hidden');
 }
